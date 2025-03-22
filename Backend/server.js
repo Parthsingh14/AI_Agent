14002555db832baa7735a7e1e0779bbefd6af701
Parth Singh <parthsingh503@gmail.com>
2025-03-22 09:29:16 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config' // ES6
+import http from 'http';
+import app from './app.js';
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+
+server.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
