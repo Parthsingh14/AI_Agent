19e7359bf875a95eb632655f5272dee2e505cff0
Parth Singh <parthsingh503@gmail.com>
2025-03-30 16:42:33 +0530

    add project management routes and controllers, refactor authentication middleware

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
 import * as userController from '../controllers/user.controller.js';
-import * as authMiddleware from '../middleware/authMiddleware.js';
+import * as authMiddleware from '../middleware/auth.middleware.js';
 
 const router = Router();
 
